ba3f067ae1d4e3967f6bc643ea1c0eb8ab998039
sher <362283977@qq.com>
2022-08-16 17:53:30 +0800

    redis1

---
+++ jt-redis/jt-redis-distributed-lock-01/src/main/java/com/emo/redis/controller/StockController.java
@@ -0,0 +1,11 @@
+package com.emo.redis.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sherxia92
+ * 2022/8/16 0016 下午 13:32
+ */
+@RestController
+public class StockController {
+}
