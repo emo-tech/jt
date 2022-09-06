e178217d90bff4deeb10ee732f82fd08b1cc38e3
elitecrm02 <elitecrm02@gmail.com>
2022-09-07 07:44:10 +0800

    mybatis interceptor

--- jt-mybatis/jt-mybatis-plugin/src/main/java/com/emo/mybatis/config/MybatisPlusConfiguration.java
+++ jt-mybatis/jt-mybatis-plugin/src/main/java/com/emo/mybatis/config/MybatisPlusConfiguration.java
@@ -1,8 +1,13 @@
 package com.emo.mybatis.config;
 
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.emo.mybatis.interceptor.PrintInterceptor;
+import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 /**
  * @author sherxia92
@@ -10,9 +15,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MybatisPlusConfiguration {
 
-
     @Bean
     public PrintInterceptor printInterceptor() {
         return new PrintInterceptor();
+    }
+
+    /**
+     * 批量处理时间问题
+     * @return
+     */
+    @Bean
+    public MetaObjectHandler metaObjectHandler() {
+        return new MetaObjectHandler() {
+            //自动添加
+            @Override
+            public void insertFill(MetaObject metaObject) {
+                //第二参数要和实体类中字段名一致，第三个参数字段类型要和实体类中字段类型一致，最后一个参数是待填入的数据
+                this.strictInsertFill(metaObject, "createdDate", Timestamp.class, Timestamp.valueOf(LocalDateTime.now()));
+            }
+
+            @Override
+            public void updateFill(MetaObject metaObject) {
+                //第二参数要和实体类中字段名一致，第三个参数字段类型要和实体类中字段类型一致，最后一个参数是待填入的数据
+                this.strictInsertFill(metaObject, "createdDate", Timestamp.class, Timestamp.valueOf(LocalDateTime.now()));
+            }
+        };
     }
 }
--- jt-mybatis/jt-mybatis-plugin/src/main/java/com/emo/mybatis/interceptor/PrintInterceptor.java
+++ jt-mybatis/jt-mybatis-plugin/src/main/java/com/emo/mybatis/interceptor/PrintInterceptor.java
@@ -2,7 +2,10 @@ package com.emo.mybatis.interceptor;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.executor.parameter.ParameterHandler;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
@@ -11,6 +14,7 @@ import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
+import java.sql.PreparedStatement;
 import java.util.Properties;
 
 /**
@@ -19,8 +23,18 @@ import java.util.Properties;
 @Slf4j
 @Data
 @Intercepts({
-        @Signature(type = Executor.class, method ="query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class }),
-
+        @Signature(type = Executor.class,
+                method ="query",
+                args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class }
+        ),
+        @Signature(type = Executor.class,
+                method ="query",
+                args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}
+        ),
+        @Signature(type = ParameterHandler.class, // 确定要拦截的对象
+                method = "setParameters", // 确定要拦截的方法
+                args = { PreparedStatement.class } // 拦截方法的参数
+        )
 })
 public class PrintInterceptor implements Interceptor {
 
